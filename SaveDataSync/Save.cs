5e7c61c18d5c52732b18d04b706b72d85a9b8bde
Zach Berritt <zberritt@gmail.com>
2021-10-17 09:29:50 -0400

    Added basic save class

--- SaveDataSync/Save.cs
+++ SaveDataSync/Save.cs
@@ -5,9 +5,20 @@ namespace SaveDataSync
 {
     public class Save
     {
-        public Save(string name, string location)
-        {
+        string Name { get; set; }
+        string Location { get; set; }
 
+        public Save(string name, string path)
+        {
+            // Validate name to prevent errors
+            if (name == null) throw new ArgumentNullException("name");
+            if (name.Length == 0) throw new ArgumentException(name, "name");
+            if (path == null) throw new ArgumentNullException("path");
+            if (path.Length == 0) throw new ArgumentException(path, "path");
+            Name = name;
+            Location = Path.GetFullPath(path);
         }
+
+
     }
 }
