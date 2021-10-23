aee0754db23e0da427511217e4cedc9ffe069b36
Zach Berritt <zberritt@gmail.com>
2021-10-22 20:48:58 -0400

    Added abstract server

---
+++ SaveDataSync/Servers/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveDataSync
+{
+    internal abstract class Server
+    {
+        public abstract string[] SaveNames();
+
+        public abstract byte[] GetSaveData(string name);
+
+        public abstract void UploadSaveData(string name, byte[] data);
+
+        public abstract bool ServerOnline();
+    }
+}
