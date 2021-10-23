aacdf70b783f36e510acb8f9c8a023a40e047617
Zach Berritt <zberritt@gmail.com>
2021-10-22 20:48:58 -0400

    Added local save list loading and saving

--- SaveDataSync/DataManagement.cs
+++ SaveDataSync/DataManagement.cs
@@ -1,16 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SaveDataSync
 {
     internal class DataManagement
     {
-        public static LocalSaveList GetLocalSaveList()
+        public static LocalSaveList GetLocalSaveList(string location)
         {
-            return new LocalSaveList();
+            if (!Directory.Exists(location) || !File.Exists(Path.Combine(location, "local_saves.json")))
+                return new LocalSaveList();
+            using (FileStream localSaveStream = File.Open(Path.Combine(location, "local_saves.json"), FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(localSaveStream))
+                {
+                    LocalSaveList localSaveList = LocalSaveList.FromJson(sr.ReadToEnd());
+                    return localSaveList;
+                }
+            }
+        }
+
+        public static void SaveLocalData(string location, LocalSaveList localSaveList)
+        {
+            // Check to see if the location exists, otherwise create it
+            if (!Directory.Exists(location)) Directory.CreateDirectory(location);
+
+            using (FileStream localSaveStream = File.Open(Path.Combine(location, "local_saves.json"), FileMode.OpenOrCreate))
+            {
+                using (StreamWriter localSaveStreamWriter = new StreamWriter(localSaveStream))
+                {
+                    localSaveStreamWriter.Write(localSaveList.ToJson());
+                }
+            }
         }
     }
 }
