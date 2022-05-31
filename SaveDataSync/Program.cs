81f4fe4d00de0e98c4316dbbc85fbf899cad3caf
Zach Berritt <zberritt@gmail.com>
2022-05-31 10:05:41 -0400

    Upgrade to .NET 6

--- SaveDataSync/Program.cs
+++ SaveDataSync/Program.cs
@@ -12,6 +12,7 @@ namespace SaveDataSync
         private static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
         }
