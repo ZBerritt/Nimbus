cf3a17523cbbb1673ece0a6edec05d4e6e8e3767
Zach Berritt <zberritt@gmail.com>
2023-02-22 21:58:29 -0500

    Fixed errors with getting saves that didn't exist

--- src/NimbusApp/Models/Servers/FileServer.cs
+++ src/NimbusApp/Models/Servers/FileServer.cs
@@ -34,21 +34,30 @@ namespace NimbusApp.Models.Servers
         public override async Task<string> GetRemoteSaveHash(string name)
         {
             Setup();
+            try {
             var path = GetSavePath(name);
             using var stream = File.OpenRead(path);
             using var sha256 = SHA256.Create();
             var hashBytes = await sha256.ComputeHashAsync(stream);
             return Encoding.UTF8.GetString(hashBytes);
+        } catch (FileNotFoundException) {
+                return null;
+            }
         }
 
         public override async Task GetSaveData(string name, string destination)
         {
             Setup();
-            var path = GetSavePath(name);
-            if (!File.Exists(destination)) throw new Exception("Destination file does not exist. Cannot retrieve data.");
-            using var remoteStream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
-            using var destinationStream = File.OpenWrite(destination);
-            await remoteStream.CopyToAsync(destinationStream);
+            try
+            {
+                var path = GetSavePath(name);
+                if (!File.Exists(destination)) throw new Exception("Destination file does not exist. Cannot retrieve data.");
+                using var remoteStream = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+                using var destinationStream = File.OpenWrite(destination);
+                await remoteStream.CopyToAsync(destinationStream);
+            }
+            catch (FileNotFoundException)
+            {}
         }
 
         public override Task<string[]> SaveNames()
@@ -80,7 +89,7 @@ namespace NimbusApp.Models.Servers
 
         private string GetSavePath(string name)
         {
-            return Path.Combine(Location, name, ".zip");
+            return Path.Combine(Location, $"{name}.zip");
         }
 
         private void Setup()
